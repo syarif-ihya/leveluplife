d6ce5fbe7e0310c81018ca8db6503ff3f6a83225
aghniyarizky <aghrzky04@gmail.com>
2025-12-16 11:46:33 +0700

    benerin bagian tambah achievement + tambah funct baru proses xp level

--- cli_app/main.py
+++ cli_app/main.py
@@ -1,11 +1,11 @@
 # print("ngerjainnya alis ngebuat appnya disini aja ges!")
 
-from gamification import add_achievement
+from gamification import add_achievement, process_achievement
 
 u = 1
-title = str(input("title:"))
 text = str(input("text:"))
 diff = int(input("diff:"))
 kat = int(input("kat:"))
 
-print(add_achievement(u, title, text, diff, kat))
+print(add_achievement(u, text, diff, kat))
+print(process_achievement(u, diff))
